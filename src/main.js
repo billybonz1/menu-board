81023c735de5ba923cf3e1b87ae01d0a9184a970
Artem Kalinichenko <ank@evogence.com>
2022-06-29 11:35:02 +0300

    new changes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PerfectScrollbar from 'vue3-perfect-scrollbar'
+import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
+
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PerfectScrollbar)
 
 app.mount('#app')
